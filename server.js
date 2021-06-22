5780ed13a40c6110b5c54ef499ba82d1ea5dae20
alisonarmstrong <alarmstrong15@gmail.com>
2021-06-22 13:21:18 -0600

    set up server and file structure

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+mongoose.set('toJSON', { virtuals: true });
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); 
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+});
